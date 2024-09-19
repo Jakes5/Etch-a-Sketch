f5fa9a36f6274dc17dec0d8cfb072fa5be63f827
JHvdSpuy <86352532+Jakes5@users.noreply.github.com>
2024-09-20 00:22:23 +0200

    adding coloring trace event - grid to be reviewed (changes upon zooming in and out in browser

--- script.js
+++ script.js
@@ -2,33 +2,33 @@ const container = document.querySelector('.container');
 // const createDiv = document.createElement('div');
 const generateBtn = document.querySelector('.generate-grid-btn');
 const inputValue = document.querySelector('#user-input');
-
+const boxes = document.querySelector('.boxes');
 let userGridInput, gridCount, cssVar;
 
 // Get the root element for CSS variables
 const CssRoot = document.querySelector(':root');
 
-const cssBasisVsGridInput ={
-    21: 1,
-    31: 3,
-    37: 4,
-    42: 5,
-    46: 6,
-    50: 7,
-    55: 8,
-    59: 9,
-    61: 10,
-    64: 11
-}
-
+// const cssBasisVsGridInput ={
+//     21: 1,
+//     31: 3,
+//     37: 4,
+//     42: 5,
+//     46: 6,
+//     50: 7,
+//     55: 8,
+//     59: 9,
+//     61: 10,
+//     64: 11
+// };
 
 function setCssVariable(userinput) {
     cssVar = setCssBasisDevider(userinput)
     // Set the value of CSS variable
     CssRoot.style.setProperty('--flexBasisDevide', cssVar);
-  }
+  };
+
 function setCssBasisDevider(input){
-    if(input <= 21 && input > 1){
+    if(input <= 19 && input > 1){
         return input + 1;
     }else if(input <= 27){
         return input + 2;
@@ -40,18 +40,20 @@ function setCssBasisDevider(input){
         return input + 5;
     }else if(input <= 46){
         return input + 6;
-    }else if(input <= 50){
+    }else if(input <= 49){
         return input + 7;
-    }else if(input <= 55){
+    }else if(input <= 51){
         return input + 8;
-    }else if(input <= 59){
+    }else if(input <= 54){
         return input + 9;
-    }else if(input <= 61){
+    }else if(input <= 57){
         return input + 10;
-    }else if(input <= 63){
+    }else if(input <= 59){
         return input + 11;
-    }else if(input === 64){
+    }else if(input <= 62){
         return input + 12;
+    }else if(input <= 64){
+        return input + 13;
     }else{
         clearGrid();
         inputValue.value = '';
@@ -64,6 +66,7 @@ function setCssBasisDevider(input){
     while (child) {
         container.removeChild(child);
         child = container.lastElementChild;
+        //https://www.geeksforgeeks.org/remove-all-the-child-elements-of-a-dom-node-in-javascript/
     }
 }
 //   setCssVariable(userinput)
@@ -84,12 +87,23 @@ generateBtn.addEventListener("click", () =>{
         createDiv.setAttribute("id", `box-${sqNo}`);
         container.appendChild(createDiv);
     };
-    // console.log(setCssBasisDevider(userGridInput));
-    // console.log(setCssVariable(userGridInput));
+    
     setCssVariable(userGridInput);
+    //https://www.w3schools.com/css/css3_variables_javascript.asp
 
+    // Colouring event
+    for(let sqNo = 1; sqNo <= gridCount; sqNo++){
+        document.querySelector(`#box-${sqNo}`).addEventListener('mouseover',()=>{
+            document.querySelector(`#box-${sqNo}`).style.backgroundColor="#04AA6D";
+        })
+    }
+
+    
     createDiv = 0;
     inputValue.value = '';
 })
 
 
+// document.querySelector('.boxes').addEventListener('mouseover',()=>{
+//     document.querySelector('.boxes').style.backgroundColor="#04AA6D";
+// })
